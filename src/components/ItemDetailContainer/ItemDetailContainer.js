491d1f99507027e80e2fac38274874407bbd92a4
Federico Walovnik <federicowalovnik@192.168.1.9>
2023-03-09 10:31:53 -0300

    url api prod

--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,39 +1,46 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, useNavigate } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom'
 
-import ItemDetail from '../ItemDetail/ItemDetail';
+import ItemDetail from '../ItemDetail/ItemDetail'
 
-import './ItemDetailContainer.scss';
-
+import config from '../../config.js'
 
 const ItemDetailContainer = () => {
-    const navigate = useNavigate();
-    const { productId } = useParams()
-    const [product, setProduct] = useState([]);
+  const navigate = useNavigate()
+  const { productId } = useParams()
+  const [product, setProduct] = useState([])
 
-    const getProduct = async () => {
-        const product = await fetch(`/api/product/${productId}`)
-        return await product.json()
-    }
-
-    const setProducts = async () => {
-        try {
-            const product = await getProduct()
-            !product ? navigate("/notFound") : setProduct(product)
-        } catch (error) {
-            console.error('error prossesing the products')
-        }
-    }
-
-    useEffect(() => {
-        setProducts()
-    }, []);
-
-    return (
-        <div className="ItemDetailContainer">
-            {product.image ? <ItemDetail details={{ id: productId, ...product }} /> : <ItemDetail loading={true} />}
-        </div >
+  const getProduct = async () => {
+    const product = await fetch(
+      `${config.url}/api/product/${productId}`
     )
+    return await product.json()
+  }
+
+  const setProducts = async () => {
+    try {
+      const product = await getProduct()
+      !product ? navigate('/notFound') : setProduct(product)
+    } catch (error) {
+      console.error('error prossesing the products')
+    }
+  }
+
+  useEffect(() => {
+    setProducts()
+  }, [])
+
+  return (
+    <div className="ItemDetailContainer">
+      {product.image ? (
+        <ItemDetail
+          details={{ id: productId, ...product }}
+        />
+      ) : (
+        <ItemDetail loading={true} />
+      )}
+    </div>
+  )
 }
 
-export default ItemDetailContainer;
+export default ItemDetailContainer
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,18 +1,16 @@
-import React, { useEffect } from 'react'
-import { useParams } from 'react-router-dom';
+import React from 'react'
+import { useParams } from 'react-router-dom'
 
 import ListItems from '../ListItems/ListItems'
 
-import './ItemListContainer.scss'
-
 const ItemListContainer = ({ title }) => {
-    const { categoryId } = useParams()
+  const { categoryId } = useParams()
 
-    return (
-        <div className="ItemListContainer">
-            <ListItems categoryId={categoryId} />
-        </div>
-    )
+  return (
+    <div className="ItemListContainer">
+      <ListItems categoryId={categoryId} />
+    </div>
+  )
 }
 
-export default ItemListContainer;
+export default ItemListContainer
