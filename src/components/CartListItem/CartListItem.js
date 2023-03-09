d8fa1e47fab0750412e39994c9e207d536d9655a
Federico Walovnik <federicowalovnik@192.168.1.9>
2023-03-09 11:19:56 -0300

    adding feature flags

--- src/components/CartListItem/CartListItem.js
+++ src/components/CartListItem/CartListItem.js
@@ -1,38 +1,55 @@
 import React from 'react'
-import { useCartContext } from "../../context/CartContext"
+import { useCartContext } from '../../context/CartContext'
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTrash } from '@fortawesome/free-solid-svg-icons';
-import { faHeart } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import { faHeart } from '@fortawesome/free-regular-svg-icons'
+
+import config from '../../config.js'
 
 import './CartListItem.scss'
 
 const CartListItem = ({ item }) => {
-    const { removeItem } = useCartContext()
+  const { removeItem } = useCartContext()
 
-    const deleteItem = () => {
-        removeItem(item.id)
-    }
+  const deleteItem = () => {
+    removeItem(item.id)
+  }
 
-    return (
-        <div className="CartItemContainer">
-            <div className="CartItem">
-                <img className="CartItem__image" src={item.image} alt={item.title} />
-                <div className="CartItem__info">
-                    <p className="CartItem__title">{item.title}</p>
-                    <p className="CartItem__data">{item.category}</p>
-                    <p className="CartItem__data">Size: 9</p>
-                    <p className="CartItem__data">Quantity: {item.quantity}</p>
-                    <div className="CartItem__icons">
-                        <FontAwesomeIcon icon={faTrash} onClick={deleteItem} />
-                        <FontAwesomeIcon icon={faHeart} />
-                    </div>
-                </div>
-                <p className="CartItem__price">${item.price * item.quantity}</p>
-            </div>
-            <span className="CartItem__divider" />
+  return (
+    <div className="CartItemContainer">
+      <div className="CartItem">
+        <img
+          className="CartItem__image"
+          src={item.image}
+          alt={item.title}
+        />
+        <div className="CartItem__info">
+          <p className="CartItem__title">{item.title}</p>
+          <p className="CartItem__data">{item.category}</p>
+          <p className="CartItem__data">Size: 9</p>
+          <p className="CartItem__data">
+            Quantity: {item.quantity}
+          </p>
+          <div className="CartItem__icons">
+            <FontAwesomeIcon
+              icon={faTrash}
+              onClick={deleteItem}
+            />
+            <FontAwesomeIcon icon={faHeart} />
+          </div>
         </div>
-    )
+        {config.showPrices ? (
+          <p className="CartItem__price">
+            ${item.price * item.quantity}
+          </p>
+        ) : (
+          <></>
+        )}
+      </div>
+      <span className="CartItem__divider" />
+    </div>
+  )
 }
 
-export default CartListItem;
+export default CartListItem
--- src/views/Checkout/CheckoutSummary/CheckoutSummary.js
+++ src/views/Checkout/CheckoutSummary/CheckoutSummary.js
@@ -1,65 +1,94 @@
-import React, { useState, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { useCartContext } from "../../../context/CartContext";
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
+import React, { useState, useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+import { useCartContext } from '../../../context/CartContext'
+import Radio from '@mui/material/Radio'
+import RadioGroup from '@mui/material/RadioGroup'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import FormControl from '@mui/material/FormControl'
 
-import "./CheckoutSummary.scss"
-import { useCheckoutContext } from "../../../context/CheckoutContext";
+import config from '../../config.js'
+
+import './CheckoutSummary.scss'
+import { useCheckoutContext } from '../../../context/CheckoutContext'
 
 const CheckoutSummary = () => {
-    const { cartList, totalPrice, emptyCart } = useCartContext()
-    const { modifyShipping } = useCheckoutContext()
+  const { cartList, totalPrice, emptyCart } =
+    useCartContext()
+  const { modifyShipping } = useCheckoutContext()
 
-    const navigate = useNavigate();
+  const navigate = useNavigate()
 
-    useEffect(() => {
-        if (cartList.length === 0) {
-            navigate("/")
-        }
-    }, [])
+  useEffect(() => {
+    if (cartList.length === 0) {
+      navigate('/')
+    }
+  }, [])
 
-    return (
-        <div className="CheckoutSummary">
-            <h2>Resumen</h2>
-            <div>
-                <div className="CheckoutSummary__products">
-                    <div className="Cart__list">
-                        {cartList.map((item) => {
-                            return (
-                                <div className="CheckoutSummary__product" key={item.title}>
-                                    <h3 className="CheckoutSummary__product-name">{item.title}</h3>
-                                    <p className="CheckoutSummary__product-price">${item.price}</p>
-                                </div>
-                            )
-                        })}
-                    </div>
-                    <div className="CheckoutSummary__totalPrice">
-                        <p className="Cart__total-title">Total</p>
-                        <p className="Cart__total-number">${totalPrice()}</p>
-                    </div>
+  return (
+    <div className="CheckoutSummary">
+      <h2>Resumen</h2>
+      <div>
+        <div className="CheckoutSummary__products">
+          <div className="Cart__list">
+            {cartList.map(item => {
+              return (
+                <div
+                  className="CheckoutSummary__product"
+                  key={item.title}
+                >
+                  <h3 className="CheckoutSummary__product-name">
+                    {item.title}
+                  </h3>
+                  {config.showPrices ? (
+                    <p className="CheckoutSummary__product-price">
+                      ${item.price}
+                    </p>
+                  ) : (
+                    <></>
+                  )}
                 </div>
-                <div className="CheckoutSummary__shipping">
-                    <FormControl>
-                        <h3>Envio</h3>
-                        <RadioGroup
-                            aria-labelledby="radio-buttons-group-shippment"
-                            defaultValue="CABA"
-                            name="radio-buttons-group"
-                            onChange={(event) => modifyShipping(event.target.value)}
-                        >
-                            <FormControlLabel value="CABA" control={<Radio />} label="Envio a domicilio CABA" />
-                            <FormControlLabel value="fueraCaba" control={<Radio />} label="Envio a domicilio fuera de Capital" />
-                            <FormControlLabel value="local" control={<Radio />} label="Retiro por el local" />
-                        </RadioGroup>
-                    </FormControl>
-
-                </div>    
-            </div>
+              )
+            })}
+          </div>
+          <div className="CheckoutSummary__totalPrice">
+            <p className="Cart__total-title">Total</p>
+            <p className="Cart__total-number">
+              ${totalPrice()}
+            </p>
+          </div>
         </div>
-    );
+        <div className="CheckoutSummary__shipping">
+          <FormControl>
+            <h3>Envio</h3>
+            <RadioGroup
+              aria-labelledby="radio-buttons-group-shippment"
+              defaultValue="CABA"
+              name="radio-buttons-group"
+              onChange={event =>
+                modifyShipping(event.target.value)
+              }
+            >
+              <FormControlLabel
+                value="CABA"
+                control={<Radio />}
+                label="Envio a domicilio CABA"
+              />
+              <FormControlLabel
+                value="fueraCaba"
+                control={<Radio />}
+                label="Envio a domicilio fuera de Capital"
+              />
+              <FormControlLabel
+                value="local"
+                control={<Radio />}
+                label="Retiro por el local"
+              />
+            </RadioGroup>
+          </FormControl>
+        </div>
+      </div>
+    </div>
+  )
 }
 
-export default CheckoutSummary;
+export default CheckoutSummary
